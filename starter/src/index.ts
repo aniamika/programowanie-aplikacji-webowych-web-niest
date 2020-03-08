367634d3b273d22cd38626a857befa3803e01bb0
Mika Anna <anna.mika@microsoft.wsei.edu.pl>
2020-03-08 18:47:46 +0100

    add functions with all behaviours

--- starter/src/index.ts
+++ starter/src/index.ts
@@ -1,62 +1,85 @@
 document.addEventListener('DOMContentLoaded', function () {
+
     const button = document.querySelector('.button');
     button.addEventListener('click', count);
+
     const apply = document.querySelector('.apply');
     apply.addEventListener('click', createElements);
+    
+    // all calculations
     function count() {
-        const luckyValue = +document.querySelector('.lucky').value;
-        const yearValue = +document.querySelector('.year').value;
-        const ageValue = +document.querySelector('.age').value;
+        const luckyValue: number = +(document.querySelector('.lucky') as HTMLInputElement).value;
+        const yearValue: number = +(document.querySelector('.year') as HTMLInputElement).value;
+        const ageValue: number = +(document.querySelector('.age') as HTMLInputElement).value;
+        
         total(luckyValue, yearValue, ageValue);
         average(luckyValue, yearValue, ageValue);
         min(luckyValue, yearValue, ageValue);
         max(luckyValue, yearValue, ageValue);
     }
+
+    // create inputs
     function createElements() {
-        const number = +document.querySelector('.inputsNumber').value;
-        for (let i = 0; i < number; i++) {
+        const number: number = +(document.querySelector('.inputsNumber') as HTMLInputElement).value;
+
+        for(let i = 0; i < number; i++) {
+            // new inputs
             const newInput = document.createElement('input');
             newInput.setAttribute('type', 'text');
             const inputsSection = document.querySelector('.inputs');
             inputsSection.appendChild(newInput);
             newInput.className = 'input';
+
+            // new delete buttons
             const newDeleteButton = document.createElement('button');
             newDeleteButton.addEventListener('click', deleteInput);
             newInput.parentNode.insertBefore(newDeleteButton, newInput.nextSibling);
             newDeleteButton.className = 'delete';
             newDeleteButton.innerHTML = 'delete';
+
+            // validation
             function check() {
-                const inputValue = +(newInput.value);
+                const inputValue: number = +(newInput.value);
                 if (Number.isNaN(inputValue)) {
                     alert('it must be a number');
-                }
+                } 
             }
             check();
+
         }
+
+
     }
+
+    // delete inputs
     function deleteInput() {
         this.previousElementSibling.remove();
         this.remove();
     }
-    function total(luckyValue, yearValue, ageValue) {
-        const total = luckyValue + yearValue + ageValue;
+
+    // function total(data: number[]) {
+    function total(luckyValue: number, yearValue: number, ageValue: number) {
+        const total: number = luckyValue + yearValue + ageValue;
         const totalResult = document.querySelector('.totalResult');
         totalResult.innerHTML = total.toString();
     }
-    function average(luckyValue, yearValue, ageValue) {
-        const average = (luckyValue + yearValue + ageValue) / 3;
+    // average
+    function average(luckyValue: number, yearValue: number, ageValue: number) {
+        const average: number = (luckyValue + yearValue + ageValue) / 3;
         const averageResult = document.querySelector('.averageResult');
         averageResult.innerHTML = average.toString();
     }
-    function min(luckyValue, yearValue, ageValue) {
-        const min = Math.min(luckyValue, yearValue, ageValue);
+    // min
+    function min(luckyValue: number, yearValue: number, ageValue: number) {
+        const min: number = Math.min(luckyValue, yearValue, ageValue);
         const minResult = document.querySelector('.minResult');
         minResult.innerHTML = min.toString();
     }
-    function max(luckyValue, yearValue, ageValue) {
-        const max = Math.max(luckyValue, yearValue, ageValue);
+    // max
+    function max(luckyValue: number, yearValue: number, ageValue: number) {
+        const max: number = Math.max(luckyValue, yearValue, ageValue);
         const maxResult = document.querySelector('.maxResult');
         maxResult.innerHTML = max.toString();
     }
+
 });
-//# sourceMappingURL=index.js.map
